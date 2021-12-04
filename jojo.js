919d2a25b20f5a7f76ef3b48c00756c5069dbb69
KodaiTakano <takano_x_kodai@icloud.com>
2021-12-04 22:03:30 +0900

    option変更、関節の角度を配列に格納

--- jojo.js
+++ jojo.js
@@ -6,21 +6,21 @@ function setup() {
     console.log('セットアップ');
     // ドキュメント内にcanvas要素を作成し、サイズをピクセル単位で設定する。
     // https://p5js.org/reference/#/p5/createCanvas
-    createCanvas(480, 640);
+    // createCanvas(480, 640);
     // createCanvas(668, 668);
     // createCanvas(298, 228);
+    createCanvas(334,500);
+
+    // var url = 'https://i0.wp.com/marutarou.com/wp-content/uploads/2020/10/o0480064012586510803.jpg?w=480&ssl=1';
+    // var url = 'https://i0.wp.com/marutarou.com/wp-content/uploads/2020/10/c20130901_jojoasbplay16_001_cs1w1_720x720.jpg?w=720&ssl=1';
+    // var url = 'https://i0.wp.com/marutarou.com/wp-content/uploads/2020/10/c20130827_jojoasbplay23_001_cs1w1_298x.jpg?resize=298%2C223&ssl=1';
+    var url = 'https://stat.ameba.jp/user_images/20151103/18/bijinzukan/e9/2d/j/t02200329_0334050013473375405.jpg?caw=800'
 
     // p5 domライブラリを使ってイメージを作成し、読み込まれたらmodelReady()を呼び出す。
     // 与えられたsrcと代替テキストでDOMに<img>要素を作成して、コンテナノードが指定されている場合にはそれに追加し、
     // 指定されていない場合には、bodyに追加する。
     // createImg(src, successCallback)
     // https://p5js.org/reference/#/p5/createImg
-    // img = createImg('jojo.jpeg', imageReady);
-
-    var url = 'https://i0.wp.com/marutarou.com/wp-content/uploads/2020/10/o0480064012586510803.jpg?w=480&ssl=1';
-    // var url = 'https://i0.wp.com/marutarou.com/wp-content/uploads/2020/10/c20130901_jojoasbplay16_001_cs1w1_720x720.jpg?w=720&ssl=1';
-    // var url = 'https://i0.wp.com/marutarou.com/wp-content/uploads/2020/10/c20130827_jojoasbplay23_001_cs1w1_298x.jpg?resize=298%2C223&ssl=1';
-
     // var img = new Image();
     // img.crossOrigin = 'anonymous';
     // img.src = url;
@@ -53,9 +53,19 @@ function imageReady() {
     console.log('イメージの準備完了');
      // オプションを設定する。
      let options = {
-        imageScaleFactor: 1,
-        minConfidence: 0.1
-    }
+        architecture: 'MobileNetV1',
+        imageScaleFactor: 0.3,
+        outputStride: 16,
+        flipHorizontal: false,
+        minConfidence: 0.5,
+        maxPoseDetections: 5,
+        scoreThreshold: 0.5,
+        nmsRadius: 20,
+        detectionType: 'multiple',
+        inputResolution: 513,
+        multiplier: 0.75,
+        quantBytes: 2,
+    };
 
     // poseNetを割り当てる。
     poseNet = ml5.poseNet(modelReady, options);
@@ -121,6 +131,7 @@ function draw() {
         image(img, 0, 0, width, height);
         drawSkeleton();
         drawKeypoints();
+        makeDegreeArray();
         // p5.jsがdraw()内のコードの連続的な実行を行うのを停める
         // https://p5js.org/reference/#/p5/noLoop
         noLoop(); // posesの推定時はループを停める
@@ -164,7 +175,7 @@ function drawKeypoints() {
                     strokeWeight(4);
                     ellipse(round(keypoint.position.x), round(keypoint.position.y), 8, 8);
                 }
-                if (12 <= j && j < 18) {
+                if (11 <= j && j < 17) {
                     fill(255,0,0);
                     stroke(20);
                     strokeWeight(4);
@@ -192,4 +203,42 @@ function drawSkeleton() {
             line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
         }
     }
+}
+
+function makeDegreeArray() {
+    degreearray = []
+    for (let i = 0; i < poses.length; i++) {
+        // 検出された各姿勢について、すべてのキーポイントを走査する。
+        let pose = poses[i].pose;
+
+        // 左肘、左肩、左腰の角度
+        degreearray.push(calcDegree(pose.keypoints[7].position, pose.keypoints[5].position, pose.keypoints[11].position))
+        // 右肘、右肩、右腰の角度
+        degreearray.push(calcDegree(pose.keypoints[8].position, pose.keypoints[6].position, pose.keypoints[12].position))
+        // 左手首、左肘、左肩の角度
+        degreearray.push(calcDegree(pose.keypoints[5].position, pose.keypoints[7].position, pose.keypoints[9].position))
+        // 右手首、右肘、右肩の角度
+        degreearray.push(calcDegree(pose.keypoints[6].position, pose.keypoints[8].position, pose.keypoints[10].position))
+        // 左肩、左腰、左膝の角度
+        degreearray.push(calcDegree(pose.keypoints[5].position, pose.keypoints[11].position, pose.keypoints[13].position))
+        // 右腰、右腰、右膝の角度
+        degreearray.push(calcDegree(pose.keypoints[6].position, pose.keypoints[12].position, pose.keypoints[14].position))
+        // 左腰、左膝、左足首の角度
+        degreearray.push(calcDegree(pose.keypoints[11].position, pose.keypoints[13].position, pose.keypoints[15].position))
+        // 右腰首、右膝、右足首の角度
+        degreearray.push(calcDegree(pose.keypoints[12].position, pose.keypoints[14].position, pose.keypoints[16].position))
+    }
+    console.log('degreearray: '+degreearray)
+}
+
+// "180"と"-180"が全然違う問題を忘れない！！！！！！！！！！！！！！！！！
+function calcDegree(a, b, c) {
+    v1 = createVector(a.x - b.x, a.y - b.y);
+    v2 = createVector(c.x - b.x, c.y - b.y);
+    // cosθ
+    cos = (v1.x * v2.x + v1.y * v2.y)/(Math.sqrt(v1.x**2 + v1.y**2) * Math.sqrt(v2.x**2 + v2.y**2));
+    // 角度=arccos(cos)*(180/π)
+    degree = Math.acos(cos) * (180 / Math.PI);
+
+    return degree;
 }
